f9e3264858ac42cab64c69a3253259c085e7c285
SHAIK AZIMUNNISA <shaikazimunnisa432@gmail.com>
2025-07-27 18:03:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -209,7 +209,7 @@ def fibsearch(arr, target):
 arr = [2, 4, 6, 8, 10, 12, 13]
 target = 10
 result = fibsearch(arr, target)
-print(f"Element {target} found at index: {result}")"""
+print(f"Element {target} found at index: {result}")
 
 
 
@@ -306,7 +306,7 @@ def pancake(arr):
     return arr
 nums = list(map(int,input("enter").split()))
 sorted_nums = pancake(nums)
-print("Sorted",sorted_nums)"""
+print("Sorted",sorted_nums)
 
 
 
@@ -334,5 +334,751 @@ print("Sorted",sorted_nums)"""
 #19.AssertionError =  
 
 
+try:
+    ----statements
+    ------------
+    ------------
+except --------:
 
-"""
+
+PROGRAM:
+
+try:
+    num1=int(input("enter numerator:"))
+    num2=int(input("enter numerator:"))
+    output=num1/num2
+    print("result:",output)
+except ZeroDivisionError:
+    print("cannot divide by zero....")
+
+
+
+try:
+    filename=input("enter the file name:")
+    file=open(filename, 'r')
+    number=int(file.readline())
+    print("number from file",number)
+    file.close()
+except  FileNotFoundError:
+    print("file does not exist....")
+except ValueError:
+    print("file does not have integer...")
+except:
+    print("unknow error....")
+
+
+
+try:
+    num=int(input("enter a number:"))
+    print(num**3)
+except (KeyboardInterrupt,ValueError,TypeError):
+    print("please check before executing....")
+print("program terminated....") 
+
+
+
+
+import math
+try:
+    num=int(input("enter a number:"))
+    if num<0:
+        raise ValueError("negative number....")
+except ValueError:
+    print("enter postive number only....")
+else:
+    print("SquareRoot:",round(math.sqrt(num),4))
+
+
+
+
+
+
+LINKED List: non contagious ds
+
+
+memory allocate
+access
+1.single ll
+2.double ll
+3.circular s/d ll
+
+SINGLE LINKED LIST:
+
+head--->node1--->node2--->tail/null
+
+class node:
+          def __init___(self,data):
+                  self.data=data
+                  self.next=none
+                  
+Insertion operations:
+
+1.at begin
+2.at end
+3.at location
+
+
+#PROGRAM: AT BEGIN
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    def iab(self,data):
+        newnode=Node(data)
+        newnode.next=self.head
+        self.head=newnode
+        print(f"{data} inserted from begin.")
+    def display(self):
+        current = self.head
+        if not current:
+            print("LL-Empty")
+            return
+        while current:
+            print(current.data, end='---')
+            current=current.next
+        print("None")
+
+ll=LinkedList()
+while True:
+    print("\n LinkedList- Insert At Begin....")
+    print("1. Insert")
+    print("2. Display")
+    print("3. Exit")
+    choice=input("Enter your choice:")
+    if choice=='1':
+        data=int(input("enter a value to insert:"))
+        ll.iab(data)
+    elif choice=='2':
+        ll.display()
+    elif choice=='3':
+        print("Exit the operation...")
+        break
+    else:
+        print("Enter only ... 1/2/3")
+
+
+
+
+#AT END:
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    def iae(self,data):
+        newnode=Node(data)
+        if self.head is None:
+            self.head=newnode
+            print(f"{data} imserted at end /will be first node")
+            return
+        current=self.head
+        while current.next:
+            current=current.next
+        current.next=newnode
+        print(f"{data} inserted at end / will be the last node")
+    def display(self):
+        current = self.head
+        if not current:
+            print("LL-Empty")
+            return
+        while current:
+            print(current.data, end='---')
+            current=current.next
+        print("None")
+
+ll=LinkedList()
+while True:
+    print("\n LinkedList- Insert At Begin....")
+    print("1. Insert")
+    print("2. Display")
+    print("3. Exit")
+    choice=input("Enter your choice:")
+    if choice=='1':
+        data=int(input("enter a value to insert:"))
+        ll.iae(data)
+    elif choice=='2':
+        ll.display()
+    elif choice=='3':
+        print("Exit the operation...")
+        break
+    else:
+        print("Enter only ... 1/2/3")
+
+
+
+#AT POSITION:
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    def iap(self,data,pos):
+        newnode=Node(data)
+        if pos<=0:
+            print("position min>=1")
+            return
+        if pos==1: #begin start
+            newnode.next=self.head
+            self.head=newnode
+            print(f"{data} inserted at pos-1")
+            return
+        current=self.head  
+        c=1
+        while current and c < pos-1:
+            current=current.next
+            c+=1
+        if not current:
+            print("not in range...")
+            return
+        newnode.next=current.next
+        current.next=newnode
+        print(f"{data} inserted at position{pos}. ")
+    def display(self):
+        current = self.head
+        if not current:
+            print("LL-Empty")
+            return
+        while current:
+            print(current.data, end='---')
+            current=current.next
+        print("None")
+
+ll=LinkedList()
+while True:
+    print("\n LinkedList- Insert At Begin....")
+    print("1. Insert")
+    print("2. Display")
+    print("3. Exit")
+    choice=input("Enter your choice:")
+    if choice=='1':
+        data=int(input("enter a value to insert:"))
+        pos=int(input("enter position starting from 1"))
+        ll.iap(data,pos)
+    elif choice=='2':
+        ll.display()
+    elif choice=='3':
+        print("Exit the operation...")
+        break
+    else:
+        print("Enter only ... 1/2/3")
+
+
+
+#ELEMENT PRESENT OR NOT IN LL:
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    def iae(self,data):
+        newnode=Node(data)
+        if not self.head:
+            self.head=newnode
+            return
+        current=self.head
+        while current.next:
+            current=current.next
+        current.next=newnode
+    def search(self,key):
+        pos=1
+        current=self.head
+        while current:
+            if current.data==key:
+                print(f"{key} found in a LL")
+                return True
+            current=current.next
+            pos+=1
+            print(f"{key} not  found in a LL")
+            
+    def display(self):
+        current = self.head
+        if not current:
+            print("LL-Empty")
+            return
+        while current:
+            print(current.data, end='---')
+            current=current.next
+        print("None")
+
+ll=LinkedList()
+while True:
+    print("\n LinkedList- Insert At Begin....")
+    print("1. Insert")
+    print("2. Display")
+    print("3.search")
+    print("4. Exit")
+    choice=input("Enter your choice:")
+    if choice=='1':
+        data=int(input("enter a value to insert:"))
+        ll.iae(data)
+    elif choice=='2':
+        ll.display()
+    elif choice=='3':
+        val=int(input("enter the value ,you want to search:"))
+        ll.search(val)
+    elif choice=='4':
+        print("Exit the operation...")
+        break
+    else:
+        print("Enter only ... 1/2/3/4")
+
+
+DELETING OPERATIONS :
+1.at end
+2.at beginning
+3.by value
+
+head--->10--->20--->30--->40--->NULL
+
+
+
+
+#DELETE OPERATION BY VALUE:
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    def iae(self,data):
+        newnode=Node(data)
+        if not self.head:
+            self.head=newnode
+            return
+        current=self.head
+        while current.next:
+            current=current.next
+        current.next=newnode    
+    def deletevalue(self,key):
+        current=self.head
+        if not current:
+            print("empty ll")
+            return
+        if current.data==key:
+            self.head=current.next
+            print(f"{key} delete from the list")
+            return
+        prev=None
+        while current and current.data!=key:
+            prev=current
+            current=current.next
+        if not current:
+            print(f"{key} not found in ll")
+            return
+        prev.next=current.next
+        print(f"{key} deleted from the ll")
+    def display(self):
+        current = self.head
+        if not current:
+            print("LL-Empty")
+            return
+        while current:
+            print(current.data, end='---')
+            current=current.next
+        print("None")
+
+ll=LinkedList()
+while True:
+    print("\n LinkedList- Insert At Begin....")
+    print("1. Insert")
+    print("2. Display")
+    print("3. delete by value")
+    print("4.exit")
+    choice=input("Enter your choice:")
+    if choice=='1':
+        data=int(input("enter a value to insert:"))
+        ll.iae(data)
+    elif choice=='2':
+        ll.display()
+    elif choice=='3':
+        key=int(input("enter the value ,you want to delete:"))
+        ll.deletevalue(key)
+    elif choice=='4':
+        print("Exit the operation...")
+        break
+    else:
+        print("Enter only ... 1/2/3/4")
+
+
+#DELETE OPERATION AT BEGIN:
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    def iae(self,data):
+        newnode=Node(data)
+        if not self.head:
+            self.head=newnode
+            return
+        current=self.head
+        while current.next:
+            current=current.next
+        current.next=newnode    
+    def deletebegin(self,key):
+        if self.head is None:
+            print("empty=ll")
+        else:
+            print("deleted node from begining:",self.head.data)
+            self.head=self.head.next
+    def display(self):
+        current = self.head
+        if not current:
+            print("LL-Empty")
+            return
+        while current:
+            print(current.data, end='---')
+            current=current.next
+        print("None")
+
+ll=LinkedList()
+while True:
+    print("\n LinkedList- Insert At Begin....")
+    print("1. Insert")
+    print("2. Display")
+    print("3. at begin")
+    print("4.exit")
+    choice=input("Enter your choice:")
+    if choice=='1':
+        data=int(input("enter a value to insert:"))
+        ll.iae(data)
+    elif choice=='2':
+        ll.display()
+    elif choice=='3':
+        ll.deletebegin(key)
+    elif choice=='4':
+        print("Exit the operation...")
+        break
+    else:
+        print("Enter only ... 1/2/3/4")
+
+
+
+#DELETE OPERATION AT END:
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    def iae(self,data):
+        newnode=Node(data)
+        if not self.head:
+            self.head=newnode
+            return
+        current=self.head
+        while current.next:
+            current=current.next
+        current.next=newnode    
+    def deleteend(self):  
+        if self.head is None:
+            print("Linked List is empty")
+        else:
+            current = self.head
+            while current.next.next:
+                current = current.next
+            print("Deleted node from end:", current.next.data)
+            current.next = None
+    print("\n LinkedList- Insert At Begin....")
+    print("1. Insert")
+    print("2. Display")
+    print("3. at end")
+    print("4.exit")
+    choice=input("Enter your choice:")
+    if choice=='1':
+        data=int(input("enter a value to insert:"))
+        ll.iae(data)
+    elif choice=='2':
+        ll.display()
+    elif choice=='3':
+        ll.deleteend()
+    elif choice=='4':
+        print("Exit the operation...")
+        break
+    else:
+        print("Enter only ... 1/2/3/4")
+
+
+
+#ATM:
+
+class Transaction:
+    def __init__(self,transaction_type,amount):
+        self.type=transaction_type
+        self.amount=amount
+        self.next=None
+class TransactionHistory:
+    def __init__(self):
+        self.head=None
+    def add_transaction(self,transaction_type,amount):
+        nn=Transaction(transaction_type,amount)
+        if not self.head:
+            self.head=nn
+        else:
+            current=self.head
+            while current.next:
+                current=current.next
+            current.next=nn
+        print(f"{transaction_type} of Rs.{amount} recorded....")
+    def show_history(self):
+        if not self.head:
+            print("No Transaction found")
+            return 
+        print("\n Transaction History ")
+        current=self.head
+        count=1
+        while current:
+            print(f"{count},{current.type}-RS{current.amount}")
+            current=current.next
+            count+=1
+history=TransactionHistory()
+while True:
+    print("\n--------ATM Transaction menu-------")
+    print("1.Deposit")
+    print("2.Withdraw")
+    print("3.History")
+    print("4.Exit")
+    choice=input("enter your choice:")
+    if choice=='1':
+        amount=float(input("enter amount to deposit:"))
+        history.add_transaction("Deposit",amount)
+    elif choice=='2':
+        amount=float(input("enter amount to withdraw:"))
+        history.add_transaction("WithDraw",amount)
+    elif choice=='3':
+        history.show_history()
+    elif choice=='4':
+        print("End of transaction.....Exit!!!")
+        break
+    else:
+        print("choose 1/2/3/4 only.....")
+
+
+
+
+CONSIDER A SINGLE LINKED LIST WITH INSERT AT THE END NODES AND
+AFTER INSERTION REVERSE THE NODE TO PRINT
+INPUT-11-22-33-44-55-NULL
+OUTPUT-55-44-33-22-11-NULL
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def insert_end(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node
+    def reverse(self):
+        prev = None
+        current = self.head
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        self.head = prev
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end="-")
+            temp = temp.next
+        print("null")
+ll = LinkedList()
+for value in [11, 22, 33, 44, 55]:
+    ll.insert_end(value)
+
+ll.reverse()
+ll.print_list()
+
+
+
+
+DOUBLE LINKED LIST:
+
+opeations
+1.insertion  -at begin,end,position
+2.deletion   -by value
+3.traversal  -forward/backward 
+
+head--->node1--->node2--->node3--->null
+
+node:
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        self.prev=None
+
+
+
+#at the end insertion at DLL:
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.prev=None
+        self.next=None
+class DoubleLinkedList:
+    def __init__(self):
+        self.head=None
+    def iae(self,data):
+        newnode=Node(data)
+        if self.head is None:
+            self.head=newnode
+            return
+        temp=self.head
+        while temp.next:
+            temp=temp.next
+        temp.next=newnode
+        newnode.prev=temp
+    def display(self):
+        temp=self.head
+        print("double linked list")
+        while temp:
+            print(temp.data,end="<->")
+            temp=temp.next
+        print("None")
+dll=DoubleLinkedList()
+n=int(input("enter a number of elements:"))
+for i in range(n):
+    val=int(input(f"enter element {i+1}:"))
+    dll.iae(val)
+dll.display()    
+
+
+
+
+# at the begin insertion at DLL:
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def iab(self, data): 
+        newnode = Node(data)
+        if self.head is None:
+            self.head = newnode
+        else:
+            newnode.next = self.head
+            self.head.prev = newnode
+            self.head = newnode
+
+    def display(self):
+        temp = self.head
+        print("Double Linked List:")
+        while temp:
+            print(temp.data, end="<->")
+            temp = temp.next
+        print("None")
+
+dll = DoubleLinkedList()
+n = int(input("Enter number of elements: "))
+for i in range(n):
+    val = int(input(f"Enter element {i+1}: "))
+    dll.iab(val)  
+dll.display()
+
+
+
+
+
+
+#at the position insertion at DLL:
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def iap(self,pos, data): 
+        newnode = Node(data)
+        if pos<=0:
+            print("invalid position")
+            return
+        if pos==1:
+            newnode.next=self.head
+            if self.head:
+                self.head.prev=newnode
+            self.head=newnode
+            return
+        temp=self.head
+        for _ in range(pos-2):
+            if temp is None:
+                print("position off range")
+                return
+            temp=temp.next
+        if temp is None:
+            print("no elements....")
+            return
+        newnode.next=temp.next
+        newnode.prev=temp
+        if temp.next:
+            temp.next.prev=newnode
+        temp.next=newnode    
+
+    def display(self):
+        temp = self.head
+        print("Double Linked List:")
+        while temp:
+            print(temp.data, end="<->")
+            temp = temp.next
+        print("None")
+
+dll = DoubleLinkedList()
+dll.iap(1,100)
+n = int(input("Enter number of elements: "))
+for i in range(n):
+    val = int(input(f"Enter element {i+1}: "))
+    pos=int(input(f"enter the position to insert {val}:"))
+    dll.iap(pos,val)  
+dll.display()
+
+
+
+
+
+
+
+
+
